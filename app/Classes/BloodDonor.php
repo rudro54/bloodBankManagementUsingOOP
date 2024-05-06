072567658b629fa19be2812e73ecc4fe0b1ac8aa
Abdullah Al Zayed <rudro54@gmail.com>
2024-05-06 16:15:29 +0600

    Blood Donor Class Complited

--- app/Classes/BloodDonor.php
+++ app/Classes/BloodDonor.php
@@ -3,8 +3,41 @@
 namespace app\Classes;
 
 
-class BloodDonor
+abstract class BloodDonor
 {
+
+    protected $name;
+    protected $bloodGroup;
+    protected $age;
+
+    public function __construct($name, $age, $bloodGroup)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->bloodGroup = $bloodGroup;
+    }
+
+    abstract public function donateBlood();
+
+
+    public function displayDonorInfo()
+    {
+
+        echo "Name  :" . $this->name . "\n";
+        echo "Age :" . $this->age . "\n";
+        echo "Blood Group :" . $this->bloodGroup . "\n";
+
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getBloodGroup()
+    {
+        return $this->bloodGroup;
+    }
 
 
 
