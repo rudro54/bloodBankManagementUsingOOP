e0a19cb116b2e47a28a405a33ae80a3539d37b0f
Abdullah Al Zayed <rudro54@gmail.com>
2024-05-06 16:58:20 +0600

    Blood Bank Class Completed Which Uses Blood Bank Interface And Blood Testing Trait

---
+++ app/Classes/BloodBank.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\Classes;
+
+use app\Interfaces\BloodBankInterface;
+use app\Traits\BloodTestingTrait;
+
+
+class BloodBank implements BloodBankInterface
+{
+
+    use BloodTestingTrait;
+
+    private $donors = [];
+
+
+
+    public function receivedBlood(BloodDonor $donor)
+    {
+        $this->donors = $donor;
+
+        echo "Blood Received From " . $donor->getName() . "\n";
+
+    }
+    public function getDonorsByBloodGroup($bloodGroup)
+    {
+
+        $matchingDonors = [];
+
+        foreach ($this->donors as $donor) {
+            if ($donor->getBloodGroup == $bloodGroup) {
+                $matchingDonors[] = $donor;
+            }
+
+        }
+
+        return $matchingDonors;
+
+
+    }
+
+
+    public function displayDonors()
+    {
+        echo "List Of Donors : \n";
+
+        foreach ($this->donors as $donor) {
+            $donor->displayDonorInfo();
+            $this->testBloodGroup($donor);
+
+            echo "\n";
+        }
+    }
+
+}
